3262e0548970e742ba24c906c7ec63f0586854eb
payammohammadi94 <payammohammadi.iran@gmail.com>
2022-07-10 13:08:02 +0430

    first commit

---
+++ src/components/Headers.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import React from 'react'
+
+
+const Container = styled.div`
+    width : 100%;
+    background-color: red;
+    height:3rem;
+`
+
+export default function Headers() {
+  return (
+    <Container>Headers</Container>
+
+  )
+}
